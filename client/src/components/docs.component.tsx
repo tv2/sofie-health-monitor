7c2898435e5ca6e2d43bc01f609f268173660e65
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-06-06 20:40:08 +0200

    Changes to docs

--- client/src/components/docs.component.tsx
+++ client/src/components/docs.component.tsx
@@ -16,11 +16,20 @@ function Docs(props: any) {
         <h3>Queries</h3>
         <pre>
           {
+          '[Options]\n' +
           'n[ame]: A sequence of characters without space and special characters.\n' +
           'r[undown]: active | inactive | rehearsal\n' +
           's[tatus]: ok | fail | warning\n' +
-          '!command: Negated command\n' +
-          'is: qbox\n'
+          'is: qbox\n' +
+          '\n' +
+          '[Operators]' +
+          '!option: Not option\n' +
+          'o1 and o2: Both options must be satisfied.\n' +
+          'o1 or o2: At least one option must be satisfied.\n' +
+          '\n' +
+          '[Misc]\n' +
+          ' - Parantheses can be used to nest options and operators.\n' +
+          ' - If no option key is defined, name is assumed.'
           }
         </pre>
       </div>
