4bc3cbf1e09099ba0380bf541be7c55569667a9f
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-06-10 16:32:34 +0200

    Updated documentation. Ready for PR.

--- client/src/components/documentation-view.component.tsx
+++ client/src/components/documentation-view.component.tsx
@@ -5,6 +5,8 @@ import './documentation-view.component.scss'
 
 import CloseLogo from '../../assets/images/x-circle.svg'
 
+import documentationText from '../documentation.json'
+
 function DocumentationView({ show, onClose }: any) {
   const dispatch = useDispatch()
   return (
@@ -17,22 +19,7 @@ function DocumentationView({ show, onClose }: any) {
       <div className="c-documentation-view__content">
         <h3>Queries</h3>
         <pre>
-          {
-          '[Options]\n' +
-          'n[ame]: A sequence of characters without space and special characters.\n' +
-          'r[undown]: active | inactive | rehearsal\n' +
-          's[tatus]: ok | fail | warning\n' +
-          'is: qbox\n' +
-          '\n' +
-          '[Operators]\n' +
-          '!option: Not option\n' +
-          'o1 and o2: Both options must be satisfied.\n' +
-          'o1 or o2: At least one option must be satisfied.\n' +
-          '\n' +
-          '[Misc]\n' +
-          ' - Parantheses can be used to nest options and operators.\n' +
-          ' - If no option key is defined, name is assumed.'
-          }
+          { documentationText.join('\n') }
         </pre>
       </div>
     </div>
